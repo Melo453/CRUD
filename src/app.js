c83640e4394b4ebea3cb035a7ac029d2e06e29af
fabri_9800 <fabrimeloni1@gmail.com>
2021-11-07 15:22:15 -0300

    conexion con db

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
+
 const app = express();
 
 //configuracion
 app.set('port', process.env.PORT || 2500);
 app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname, 'views'));
 
-//vistas
-
-
+app.use(morgan('dev'));
+app.use(myConnection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'pruebaCrud'
+}, 'single'));
 
 
 
